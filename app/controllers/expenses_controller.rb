788db2f50efb7dbd001d6e44aa2f8ec4054df102
andres-martin <andresmartin266@gmail.com>
2019-04-27 17:22:33 -0500

    expenses controller validations added, sing_in/sign_up views fixed

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,10 @@
 class ExpensesController < ApplicationController
   before_action :authenticate_user!
   def index
+    redirect_to sign_in_path unless user_signed_in?  
+    
     @expenses = Expense.order("date DESC")
+    @expenses = Expense.where("user_id = ?", current_user)
     @expenses= @expenses.where(category_id: params[:category_id]) unless !params[:category_id].present?   
     @expenses= @expenses.where('concept LIKE ?',"%#{params[:concept]}%") unless !params[:concept].present?    
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'expenses#index'
-  
+    
+  devise_for :users
   resources :expenses, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
